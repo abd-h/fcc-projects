79049b9105c0921c2720580c8b75ef7a070bf9c7
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2022-06-22 22:58:37 +0100

    completed optimising thecode  cash register project

--- javascript-algorithm-and-data-structure-projects/cash-register.js
+++ javascript-algorithm-and-data-structure-projects/cash-register.js
@@ -26,144 +26,79 @@
     
     See below for an example of a cash-in-drawer array: */
 
-   const cashInDrawer = [
-        ["PENNY", 1.01],
-        ["NICKEL", 2.05],
-        ["DIME", 3.1],
-        ["QUATER", 4.25],
-        ["ONE", 90],
-        ["FIVE", 55], 
-        ["TEN", 20],
-        ["TWENTY", 60],
-        ["ONE HUNDRED", 100]
-    ]
+      // Workout for the total cash in the drawer
+      function totalFloat(cid) {
+            return cid.reduce((total, money) => {
+                return total + money[1];
+            }, 0)
+      }
 
-    /* This function return the total of the till float */
-    function totalCashInDrawer(cid) {
-        return cid.reduce((total, money) => {
-            total += money[1];
-            return (total);
-        }, 0)
-    }
+    function  checkCashRegister(price, cash, cid) {
+      let cashInDrawer = cid.slice(),
+           float = totalFloat(cashInDrawer),
+           changeDue = cash - price,
+           result = {status: "", change: []};
 
-
-function checkCashRegister(price, cash, cid) {
-    let cashInTill = cid.slice(),
-        totalCashInTill =  parseFloat(totalCashInDrawer(cashInTill)),
-        changeDue = cash - price,
-        penny = 0.01,
-        nickel = 0.05,
-        dime = 0.1,
-        quarter = 0.25,
-        dollar = 1,
-        five = 5,
-        ten = 10,
-        twenty = 20,
-        oneHundred = 100,
-        finalChange,
-        result = {
-            status: "", 
-            change: []
-        };
-    if(changeDue > totalCashInTill) {
-        result.status = "INSUFFICIENT_FUNDS";
-        return result;
-    }
-    if(changeDue === totalCashInTill) {
-        cashInTill.filter((money) => {
-            if(money.includes("TWENTY")
-                || money.includes("TEN")
-                || money.includes("FIVE")
-                || money.includes("ONE") 
-                || money.includes("QUARTER")
-                || money.includes("DIME") 
-                || money.includes("NICKEL") 
-               || money.includes("ONE HUNDRED")
-            || money.includes("PENNY") && money[1] > 0.01) {
-              result.status = "CLOSED";
-              result.change.push(money);
-              
-            }
-        })
-    } if(changeDue < dollar && totalCashInTill > oneHundred) {
-        let float = cashInTill.slice();
-        float.map(money => {
-            if(money.includes("QUARTER")) {
-                money.pop()
-                money.push(changeDue);
-                result.status = "OPEN",
-                result.change.push(money);
-            }
-        })
-    }
-    
-    
-    else{
-        if(changeDue < totalCashInTill) {
-            cashInTill.filter(money => {
-            if(money.includes("TWENTY")
-                || money.includes("TEN")
-                || money.includes("FIVE")
-                || money.includes("ONE") && money[1] >= 1
-                || money.includes("QUARTER")
-                || money.includes("DIME") 
-                || money.includes("NICKEL") 
-               || money.includes("ONE HUNDRED")
-            || money.includes("PENNY") && money[1] >= 0.01) {
+           if(float < changeDue) {
                 result.status = "INSUFFICIENT_FUNDS";
-            }
-            })
-        } if(changeDue < oneHundred && changeDue < totalCashInTill && totalCashInTill > 2) {
-        cashInTill.reverse().filter((money) => {
-               if(changeDue <100 && money[1] < oneHundred) {
-                    if(money[1] % twenty == 0) {
-                       finalChange = changeDue - money[1] ;
-                        result.status = "OPEN";
-                        result.change.push(money);
-                        return changeDue = finalChange;
-                    }else if(changeDue < twenty) {
-                           if(money.includes("FIVE")) {
-                            money.pop();
-                            money.push(parseInt(((changeDue - parseFloat((changeDue % 5).toFixed(2))).toFixed())));
-                            result.change.push(money)
-                            finalChange = changeDue - parseInt(((changeDue - parseFloat((changeDue % 5).toFixed(2))).toFixed()));
-                            changeDue = finalChange;
-                           }
-                           if(money.includes("ONE")) {
-                            money.pop();
-                            money.push(parseInt(((changeDue - parseFloat((changeDue % 1).toFixed(2))).toFixed())));
+           } else if(float > changeDue && float < 2) {
+               cashInDrawer.filter((money, i, array) => {
+                // console.log(money);
+                    if(money.includes("ONE") && money[1] == 1
+                        || money.includes("QUARTER") && money[1] == 0) { 
+                        result.status = "INSUFFICIENT_FUNDS";
+                    } 
+               })
+              
+           } else if(float == changeDue) {
+                result.status = "CLOSED",
+                result.change.push(...cashInDrawer)
+           } else if(float > changeDue && float > 100 && changeDue < 1) {
+                result.status = "OPEN",
+                result.change.push(["QUARTER", 0.5])
+           } else if(float > changeDue && float > 2) {
+                cashInDrawer.reverse().filter(money => {
+                    if(changeDue < 100 && money[1]  < 100) {
+                        if(money[1] % 20 == 0) {
+                            result.status = "OPEN",
                             result.change.push(money);
-                            finalChange = changeDue - parseInt(((changeDue - parseFloat((changeDue % 1).toFixed(2))).toFixed()));
-                            changeDue = finalChange;
-                           }
-                           if(money.includes("QUARTER")) {
-                            money.pop();
-                            money.push(parseFloat(parseFloat(changeDue - (parseFloat((changeDue % 0.25).toFixed(2)))).toFixed(2)));
-                            result.change.push(money);
-                            finalChange = changeDue - parseFloat(parseFloat(changeDue - (parseFloat((changeDue % 0.25).toFixed(2)))).toFixed(2));
-                            changeDue = finalChange
-                           }
-                          if(money.includes("DIME")) {
-                            money.pop();
-                            money.push(parseFloat(parseFloat(changeDue - (parseFloat((changeDue % 0.1).toFixed(2)))).toFixed(2)));
-                            result.change.push(money);
-                            finalChange = changeDue - parseFloat(parseFloat(changeDue - (parseFloat((changeDue % 0.1).toFixed(2)))).toFixed(2));
-                            changeDue = finalChange;
-                          }
-                            if(money.includes("PENNY") && changeDue < 0.5) {
+                            changeDue -= money[1];
+                        } else if(changeDue < 20) {
+                            if(money.includes("FIVE")) {
                                 money.pop();
-                                money.push(parseFloat(parseFloat(changeDue - (parseFloat((changeDue % 0.01).toFixed(1)))).toFixed(2)));
+                                money.push(parseInt(((changeDue - parseFloat((changeDue % 5).toFixed(2))).toFixed())));
+                                result.change.push(money)
+                                changeDue -= parseInt(((changeDue - parseFloat((changeDue % 5).toFixed(2))).toFixed()));
+                            } else if(money.includes("ONE")) {
+                                money.pop();
+                                money.push(parseInt(((changeDue - parseFloat((changeDue % 1).toFixed(2))).toFixed())));
+                                changeDue -= parseInt(((changeDue - parseFloat((changeDue % 1).toFixed(2))).toFixed()));
                                 result.change.push(money);
+                            } else if(changeDue < 1 && money.includes("QUARTER")) {
+                                money.pop();
+                                money.push(parseFloat(parseFloat(changeDue - (parseFloat((changeDue % 0.25).toFixed(2)))).toFixed(2)));
+                                result.change.push(money);
+                                changeDue -= parseFloat(parseFloat(changeDue - (parseFloat((changeDue % 0.25).toFixed(2)))).toFixed(2));
+                            } else if(money.includes("DIME")) {
+                                money.pop();
+                                money.push(parseFloat(parseFloat(changeDue - (parseFloat((changeDue % 0.1).toFixed(2)))).toFixed(2)));
+                                changeDue -= parseFloat(parseFloat(changeDue - (parseFloat((changeDue % 0.1).toFixed(2)))).toFixed(2));
+                                result.change.push(money)
+                            } else if(changeDue < 0.05 && money.includes("PENNY")) {
+                                money.pop();
+                                money.push(parseFloat(parseFloat(changeDue - (parseFloat((changeDue % 0.01).toFixed(1)))).toFixed(2)))
+                                changeDue -= parseFloat(parseFloat(changeDue - (parseFloat((changeDue % 0.01).toFixed(1)))).toFixed(2));
+                                result.change.push(money)
                             }
+                        }
                     }
-               }
-            })
-        } 
-         
-       
-    } 
-    return result;
-}
+                })
+           }
+           return result;
+    }
+
+
+
 
 
 
