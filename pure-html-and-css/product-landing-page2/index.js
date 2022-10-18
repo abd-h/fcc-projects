d05b3f870b268ad1f1dd54e78561ab81b283b9ab
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2022-10-19 00:33:09 +0100

    Added WAtch Card 4

--- pure-html-and-css/product-landing-page2/index.js
+++ pure-html-and-css/product-landing-page2/index.js
@@ -6,7 +6,7 @@ let mainImage = document.getElementsByClassName("chosen-watch")[0],
     watch1Option1 = document.getElementsByClassName("shadow-grey")[0],
     watch1Option2 = document.getElementsByClassName("lunar-white")[0],
     watch1Option3 = document.getElementsByClassName("blue-mist")[0],
-    outline = `3px solid rgb(193, 192, 192)`;
+    outline = `2px solid rgb(193, 192, 192)`;
 
 watch1Option1.addEventListener("click", () => {
     mainImage.src = `images/sense2/shadow-grey/sense2-black-device-3qt.png`;   
@@ -84,7 +84,8 @@ let watchCollection2 = document.querySelector(".watch-2"),
     shopNow2 = document.querySelector(".shop2");
 
 watchCollection2.addEventListener("mouseenter", () => {
-    document.querySelector(".shop2").style.display = "flex";
+    shopNow2.style.display = "flex";
+    shopNow2.style.fontSize = "0.5rem";
 })
 watchCollection2.addEventListener("mouseleave", () => {
     document.querySelector(".shop2").style.display = "none";
@@ -92,3 +93,53 @@ watchCollection2.addEventListener("mouseleave", () => {
     watch2Option1.style.outline = `none`;
     watch2Option2.style.outline = `none`;
 })    
+
+            /*++++++++++++++++++++++++++++++++++++++++
+                            Watch 3
+            *++++++++++++++++++++++++++++++++++++++++*/
+
+let watch3Options = document.querySelector(".watch-1-option-1"),
+    watch3Option1 = document.querySelector(".obsidian-matte-black"),
+    watch3Option2 = document.querySelector(".hazel-champagne-gold"),
+    watch3Option3 = document.querySelector(".chalk-polished-silver");
+
+
+
+    watch3Option1.addEventListener("click", () => {
+    watch3Options.src = 'images/gpixle-watch-4g-lite/obsidian/pixel-watch-obsidian.png';
+    watch3Option1.alt = `Obsidian color option`
+    watch3Option1.style.outline = outline;
+    watch3Option2.style.outline = `none`;
+    watch3Option3.style.outline = `none`;
+});
+
+    watch3Option2.addEventListener("click", () => {
+    watch3Options.src = `images/gpixle-watch-4g-lite/gg-pixel-hazel/google-pixel-watch-hazel-device-3qt.png`;
+    watch3Option2.style.outline = outline;
+    watch3Option1.style.outline = `none`;
+    watch3Option3.style.outline = `none`;
+});
+
+    watch3Option3.addEventListener("click", () => {
+    watch3Options.src = `images/gpixle-watch-4g-lite/silver-polish/pixel-watch-charcoal.png`;
+    watch3Option3.style.outline = outline;
+    watch3Option1.style.outline = `none`;
+    watch3Option2.style.outline = `none`;
+});
+
+let watchCollection3 = document.querySelector(".watch-3"),
+    shopNow3 = document.querySelector(".shop3");
+
+watchCollection3.addEventListener("mouseenter", () => {
+    shopNow3.style.display = "flex";
+    shopNow3.style.fontSize = "0.5rem";
+})
+watchCollection3.addEventListener("mouseleave", () => {
+    shopNow3.style.display = "none";
+    watch3Option3.style.outline = `none`;
+    watch3Option1.style.outline = `none`;
+    watch3Option2.style.outline = `none`;
+})   
+
+
+                           
